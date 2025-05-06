1d51d327a61d6f177376b1e4272e07df41159ce9
sam9999999999999 <kali.nux166@gmail.com>
2025-05-07 02:07:43 +0530

    Create pules_code_modulation.py
    under constraction

---
+++ pules_code_modulation.py
@@ -0,0 +1,68 @@
+import math
+#A = amplitude
+#T = time
+#w = frequency
+#𝜎 = phase
+# 14553
+
+#   Analog Signal Generator
+
+
+def analog_signal_generator(A, omega, sigma, duration, t):
+    signalgen = round(A*math.sin(omega*t + sigma),2)
+    #print(signalgen)
+    return signalgen
+
+def sampler(samples, interval, A, omega, sigma, duration):
+    t=0
+    while t <= duration:
+        samples.append(analog_signal_generator(A, omega, sigma, duration, t))
+        t += 1
+
+def quantizer(samples, pcmpulses, levels):
+    A_max = max(samples)
+    A_min = min(samples)
+    for i in samples:
+        Q_level = round(((i - (A_min)) / (A_max - A_min) * levels), 0)
+
+        if round(A_max) < Q_level:
+            Q_level = round(A_max)
+            pcmpulses.append(Q_level)
+        else:
+            Q_level = int(Q_level)
+            pcmpulses.append(Q_level)
+        print(pcmpulses)
+
+
+def encoder(pcmpulses, encoderbits):
+    dsignal = []
+    for x in pcmpulses:
+        x=int(x)
+        print(x)
+        bits = format(x, 'b')
+        bits = bits.zfill(encoderbits)
+        dsignal.append(bits)
+    print(dsignal)
+
+
+
+
+
+
+A = float(input("Amplitude (A): "))
+omega = float(input("Angular Frequency (omega): "))
+sigma = float(input("Phase Shift (sigma): "))
+duration = float(input("Duration (seconds): "))
+interval = float(input("Sampling Interval (seconds): "))
+encoderbits = int(input("Encoder Bits: "))
+levels = 2**encoderbits
+
+samples = []
+pcmpulses = []
+
+sampler(samples, interval, A, omega, sigma, duration)
+quantizer(samples, pcmpulses, levels)
+encoder(pcmpulses, encoderbits)
+# while t <= duration:
+#     analog_signal_generator(A, omega, sigma, duration, t)
+#     t += 1
